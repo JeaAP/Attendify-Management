1321b75fd50908f5a241045aab5e2900a687d89d
Jean <jeanarby030108@gmail.com>
2025-02-23 21:32:10 +0700

    db nya

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 if(!defined('DB_HOST')) define('DB_HOST', 'localhost');
-if(!defined('DB_USER')) define('DB_USER', 'root');
-if(!defined('DB_PASS')) define('DB_PASS', '');
+if(!defined('DB_USER')) define('DB_USER', 'bacs5153');
+if(!defined('DB_PASS')) define('DB_PASS', 'k1QwiSmarvUx27');
 if(!defined('DB_NAME')) define('DB_NAME', 'bacs5153_attendify');
 //DB_USER = 'bacs5153'
 //DB_PASS = 'k1QwiSmarvUx27'
